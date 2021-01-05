0863f042d66e7f7e9edcf1b8862b011f3f6ec310
Júlio César <jc10ferreira@gmail.com>
2021-01-05 11:40:22 -0300

    added option to not create a secong account if its already registered

--- app/Http/Controllers/JWTAuthController.php
+++ app/Http/Controllers/JWTAuthController.php
@@ -41,6 +41,14 @@ class JWTAuthController extends Controller
             if ($validator->fails()) {
                 return response($validator->errors(), 422);
             } else {
+                $user = User::where('email', '=', $request->email)->first();
+
+                if ($user === null) {
+                    return response()->json([
+                        'error' => 'Usuário já existente em nossa plataforma.'
+                    ], 409);
+                }
+
                 $user = User::create(array_merge(
                     $validator->validated(),
                     ['password' => bcrypt($request->password)]
