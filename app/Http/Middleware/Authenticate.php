80a6ebed9d0639e57f811e5c071ec824c2f4df12
Júlio César <jc10ferreira@gmail.com>
2021-06-05 01:12:08 -0300

    ajuste no erro ao não ter token

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (!$request->expectsJson()) {
-            return dd('Unauthenticated.');
+            return response()->json(['status' => 'Token is Invalid']);
         }
     }
 }
